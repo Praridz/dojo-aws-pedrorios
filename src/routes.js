4afaee44db61b96ddd47893cc84d08dde77d624a
Praridz <rios.371@hotmail.com>
2020-02-29 04:46:19 -0500

    dojo aws pedrorios

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const getTodos = require('./services/get-todos');
+
+router.get('/todos',getTodos);
+
+
+
+
+
+
+
+
+
+module.exports = router;
